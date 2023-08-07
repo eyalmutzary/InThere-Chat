a6ca927ad146cc0e9f287583f54e55625aacb0f9
giladomesi <gilad1016@gmail.com>
2023-08-07 21:01:16 +0300

    map

--- client/src/components/EventFlow/components/MyMap.js
+++ client/src/components/EventFlow/components/MyMap.js
@@ -41,6 +41,28 @@ const Input = styled.input`
   margin: 12px;
 `;
 
+const List = styled.ul`
+  width: 90%;
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ListItem = styled.li`
+  cursor: pointer;
+  margin: 4px;
+  padding: 4px 12px;
+  background-color: ${({theme}) => theme.colors.unique1};
+  border-radius: 20px;
+
+
+  &:hover {
+    background-color: ${({theme}) => theme.colors.unique2};
+  }
+`;
+
 const IconPerson = new L.Icon({
   iconUrl: require("../../../assets/map_icon.png"),
   popupAnchor: [12, 0]
@@ -54,6 +76,7 @@ export default function MyMap() {
   const [longitude, setLongitude] = useState(localStorage.getItem('longitude') || 0);
   const [address, setAddress] = useState("");
   const [markerPosition, setMarkerPosition] = useState([latitude, longitude]);
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -70,12 +93,12 @@ export default function MyMap() {
     );
   }, []);
 
-  function handleMapClick(e) {
-    const {lat, lng} = e.latlng;
+  const handleMarkerDrag = (e) => {
+    const {lat, lng} = e.target.getLatLng();
     setMarkerPosition([lat, lng]);
     setLatitude(lat);
     setLongitude(lng);
-  }
+  };
 
   const handleSearch = async () => {
     try {
@@ -86,15 +109,33 @@ export default function MyMap() {
 
       console.log(response);
       if (response.status.code === 200 && response.results.length > 0) {
-        const {geometry} = response.results[0];
-        setMarkerPosition([geometry.lat, geometry.lng]);
-        setLatitude(geometry.lat);
-        setLongitude(geometry.lng);
+        const results = response.results.slice(0, 3); // Take the top 3 results
+        setSearchResults(results);
       } else {
         console.log('No results found for the search query.');
+        setSearchResults([]);
       }
     } catch (error) {
       console.error('Error searching for the location:', error);
+      setSearchResults([]);
+    }
+  };
+
+  const handleSelectLocation = (selectedLocation) => {
+    const {geometry} = selectedLocation;
+    setMarkerPosition([geometry.lat, geometry.lng]);
+    setLatitude(geometry.lat);
+    setLongitude(geometry.lng);
+    setAddress("");
+    setSearchResults([]); // Clear search results after selecting a location
+  };
+
+  const handleInputChange = (e) => {
+    setAddress(e.target.value);
+    setSearchResults([]); // Clear search results when typing
+
+    if (e.target.value.length >= 3) {
+      handleSearch(); // Trigger search only when 3 or more characters are typed
     }
   };
 
@@ -103,23 +144,31 @@ export default function MyMap() {
       <Input
         placeholder="Search for a location"
         value={address}
-        onChange={(e) => setAddress(e.target.value)}
-        onBlur={handleSearch} // Trigger search when the user leaves the input field
+        onChange={handleInputChange} // Trigger search after every character typed
       />
 
-      <Container center={[latitude, longitude]} zoom={14} scrollWheelZoom={true} whenCreated={setMap}
-                 onClick={handleMapClick}>
-        {/*<SearchMap />*/}
+      {searchResults.length > 0 && (
+        <List>
+          {searchResults.map((result) => (
+            <ListItem key={result.annotations.geohash} onClick={() => handleSelectLocation(result)}>
+              {result.formatted}
+            </ListItem>
+          ))}
+        </List>
+      )}
+
+      <Container center={markerPosition} zoom={14} scrollWheelZoom={true} whenCreated={setMap}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
         />
-        <Marker position={markerPosition} icon={IconPerson}>
+        <Marker position={markerPosition} icon={IconPerson} draggable={true}
+                eventHandlers={{dragend: handleMarkerDrag}}>
           {/*<Popup>*/}
           {/*  A marker here!*/}
           {/*</Popup>*/}
         </Marker>
       </Container>
     </Wrapper>
-  )
+  );
 }
