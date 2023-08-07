deb7e83e32bc5dd3555372ed9c8227bb75c41af3
giladomesi <gilad1016@gmail.com>
2023-08-07 22:42:23 +0300

    map

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
-
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 
 import App from './App';
 
-createRoot(document.getElementById('root')).render(<App />);
+const meta = document.createElement('meta');
+meta.name = 'viewport';
+meta.content = 'width=device-width, initial-scale=1, shrink-to-fit=no';
+document.head.appendChild(meta);
+
+createRoot(document.getElementById('root')).render(<App/>);
