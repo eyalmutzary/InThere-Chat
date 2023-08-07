b8c593adb309832f2c61a45fa1a5a4aa0910d88b
giladomesi <gilad1016@gmail.com>
2023-08-08 01:59:28 +0300

    profile edit

--- client/src/components/Profile/EditProfile.js
+++ client/src/components/Profile/EditProfile.js
@@ -1,25 +1,44 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
-import { Button, Form, Input, Select, Space } from 'antd';
-import { Icon } from '../shared';
-import { useNavigate, useSearchParams } from 'react-router-dom';
-import { firestore } from '../../firebase';
+import {Button, Form, Input, Select, Space} from 'antd';
+import {Icon} from '../shared';
+import {useNavigate, useSearchParams} from 'react-router-dom';
+import {firestore} from '../../firebase';
 
 
 const FormWrapper = styled.div`
-    padding: 10px 30px 30px 30px;
-    background-color: ${({ theme }) => theme.colors.container};
+  padding: 10px 30px 30px 30px;
+  background-color: ${({theme}) => theme.colors.background};
+  height: 100vh;
+  overflow: auto;
 `;
 
 const Text = styled.div`
-    font-size: 20px;
+  font-size: 20px;
 `;
 
 const BackIcon = styled(Icon)`
   font-size: 30px;
-  color: ${({ theme }) => theme.colors.black};
+  color: ${({theme}) => theme.colors.black};
   margin: 20px 0 0 20px;
 `;
+
+const FormItem = styled(Form.Item)`
+  background-color: ${({theme}) => theme.colors.container};
+  border-radius: 10px;
+  padding: 12px;
+`;
+
+const ColoredInput = styled(Input)`
+  background-color: ${({theme}) => theme.colors.subContainer};
+  border-radius: 10px;
+  border: none;
+  font-size: 20px;
+  color: ${({theme}) => theme.colors.mainText};
+  margin: 8px;
+  width: 80%;
+`;
+
 
 const HOBBIES_DUMMY_DATA = [{
   label: 'Basketball',
@@ -137,52 +156,67 @@ const EditProfile = () => {
         
         <FormWrapper>
           <BackIcon name="arrow-left" onClick={() => navigate(-1)} />
-            {isLoading ? <div>Loading...</div> : 
-            <Form form={form} autoComplete="off" onFinish={onFinish}>
-                <Form.Item 
-                    name="phone"
+            {isLoading ? <div>Loading...</div> :
+              <Form form={form} autoComplete="off" onFinish={onFinish}>
+                <FormItem
+                  name="phone"
                 >
-                    <Text>Phone Number:</Text>
-                    <Input size={'large'} style={{ height: 50, fontSize: 20 }} defaultValue={userDetails.phone} onChange={(e) => setFormState((oldState) => {return {...oldState, phone: e.target.value}})}/>
-                </Form.Item>
-                <Form.Item
-                    name="instagramUrl"
+                  <Text>Phone Number:</Text>
+                  <ColoredInput size={'large'} style={{height: 50, fontSize: 20}} defaultValue={userDetails.phone}
+                                onChange={(e) => setFormState((oldState) => {
+                                  return {...oldState, phone: e.target.value}
+                                })}/>
+                </FormItem>
+                <FormItem
+                  name="instagramUrl"
                 >
-                    <Text>Instagram URL:</Text>
-                    <Input size={'large'} style={{ height: 50, fontSize: 20 }} defaultValue={userDetails.instagramUrl} onChange={(e) => setFormState((oldState) => {return {...oldState, instagramUrl: e.target.value}})}/>
-                </Form.Item>
-                <Form.Item
-                    name="facebookUrl"
+                  <Text>Instagram URL:</Text>
+                  <ColoredInput size={'large'} style={{height: 50, fontSize: 20}}
+                                defaultValue={userDetails.instagramUrl} onChange={(e) => setFormState((oldState) => {
+                    return {...oldState, instagramUrl: e.target.value}
+                  })}/>
+                </FormItem>
+                <FormItem
+                  name="facebookUrl"
                 >
-                    <Text>Facebook URL:</Text>
-                    <Input size={'large'} style={{ height: 50, fontSize: 20 }} defaultValue={userDetails.facebookUrl} onChange={(e) => setFormState((oldState) => {return {...oldState, facebookUrl: e.target.value}})}/>
-                </Form.Item>
-                    <Form.Item
-                    name="about"
-                    >
-                    <Text>About:</Text>
-                    <Input.TextArea maxLength={200} size={'large'} style={{ height: 180, fontSize: 20 }} defaultValue={userDetails.about} onChange={(e) => setFormState((oldState) => {return {...oldState, about: e.target.value}})}/>
-                </Form.Item>
+                  <Text>Facebook URL:</Text>
+                  <ColoredInput size={'large'} style={{height: 50, fontSize: 20}} defaultValue={userDetails.facebookUrl}
+                                onChange={(e) => setFormState((oldState) => {
+                                  return {...oldState, facebookUrl: e.target.value}
+                                })}/>
+                </FormItem>
+                <FormItem
+                  name="about"
+                >
+                  <Text>About:</Text>
+                  <ColoredInput.TextArea maxLength={200} size={'large'} style={{height: 180, fontSize: 20}}
+                                         defaultValue={userDetails.about} onChange={(e) => setFormState((oldState) => {
+                    return {...oldState, about: e.target.value}
+                  })}/>
+                </FormItem>
 
-                <Form.Item>
-                    <Text>Intrests:</Text>
-                    <Select
-                        size={'large'}
-                        mode="multiple"
-                        style={{ width: '100%', fontSize: 20 }}
-                        placeholder="Please select"
-                        defaultValue={userDetails?.intrests ?? []}
-                        onChange={(interstsList) => setFormState((oldState) => {return {...oldState, intrests: interstsList}})}
-                        options={HOBBIES_DUMMY_DATA}
-                    /> 
-                </Form.Item>
+                <FormItem>
+                  <Text>Intrests:</Text>
+                  <Select
+                    size={'large'}
+                    mode="multiple"
+                    style={{width: '100%', fontSize: 20}}
+                    placeholder="Please select"
+                    defaultValue={userDetails?.intrests ?? []}
+                    onChange={(interstsList) => setFormState((oldState) => {
+                      return {...oldState, intrests: interstsList}
+                    })}
+                    options={HOBBIES_DUMMY_DATA}
+                  />
+                </FormItem>
 
-                <Form.Item>
-                    <Space>
-                        <Button type="primary" htmlType="submit" style={{ width: 100, height: 60, fontSize: 20, backgroundColor: "#9381ff"}}>Submit</Button>
-                        {/* <Button htmlType="reset" style={{ width: 100, height: 60, fontSize: 20 }}>Reset</Button> */}
-                    </Space>
-                </Form.Item>
+                <FormItem>
+                  <Space>
+                    <Button type="primary" htmlType="submit"
+                            style={{width: 100, height: 60, fontSize: 20, backgroundColor: "#9381ff"}}>Submit</Button>
+                    {/* <Button htmlType="reset" style={{ width: 100, height: 60, fontSize: 20 }}>Reset</Button> */}
+                  </Space>
+                </FormItem>
             </Form>}
         </FormWrapper>
     </>
