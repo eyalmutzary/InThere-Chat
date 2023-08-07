aeec64a3acea7583405860b53ca356c48e5cc77f
giladomesi <gilad1016@gmail.com>
2023-08-07 23:19:54 +0300

    map

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import {createRoot} from 'react-dom/client';
 
 import App from './App';
 
-const meta = document.createElement('meta');
-meta.name = 'viewport';
-meta.content = 'width=device-width, height=device-height, initial-scale=1';
-document.head.appendChild(meta);
+// const meta = document.createElement('meta');
+// meta.name = 'viewport';
+// meta.content = 'width=device-width, height=device-height, initial-scale=1';
+// document.head.appendChild(meta);
 
 createRoot(document.getElementById('root')).render(<App/>);
